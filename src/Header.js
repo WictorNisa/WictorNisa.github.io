ad1b037b380788f399072b3123d763eafbb49102
WictorNisa <wictornisa@hotmail.com>
2020-12-19 16:48:25 +0100

    init

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function header() {
+     return (
+         <section className="hero-section">
+            <h1>hello horungar</h1>
+         </section>
+     );
+
+}
+    export default header;
