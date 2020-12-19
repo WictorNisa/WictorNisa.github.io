e9b8874f770eb12ee5015df8c78ce1115362c5cd
WictorNisa <wictornisa@hotmail.com>
2020-12-19 19:11:56 +0100

    Added hero section

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,21 @@
-import React from 'react';
-
+import React from "react";
 
 function header() {
-     return (
-         <section className="hero-section">
-            <h1>hello horungar</h1>
-         </section>
-     );
-
+  return (
+    <section className="hero-section">
+      <div className="hero-overlay">
+        <header></header>
+        <div className="hero-text-contain">
+          <h1 className="h1-hero-left slide-in-blurred-left">Wictor</h1>
+          <h1 className="h1-hero-right slide-in-blurred-right">Niså</h1>
+        </div>
+        <div className="welcome-contain fade-in">
+            <p className="welcome-p">Front-End Developer with a huge interest in design. Take a look at my
+            recent projects</p>
+            <button className="project-button">Check it out</button>
+        </div>
+      </div>
+    </section>
+  );
 }
-    export default header;
+export default header;
