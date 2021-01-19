8e2e7988f9b96c727241b205c1be5626dbe744fd
WictorNisa <wictornisa@hotmail.com>
2021-01-19 20:23:48 +0100

    Save

--- src/Header.js
+++ src/Header.js
@@ -7,6 +7,14 @@ function header() {
     <section className="hero-section" id="home">
      <Hamburger/>
       <div className="hero-overlay">
+      <nav className="slide-in-blurred-top">
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="#aboutme">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+    </nav>
         <div className="hero-text-contain">
           <h1 className="h1-hero-left slide-in-blurred-left">Wictor</h1>
           <h1 className="h1-hero-right slide-in-blurred-right">Niså</h1>
@@ -14,7 +22,7 @@ function header() {
         <div className="welcome-contain fade-in">
             <p className="welcome-p">Front-End Developer with a huge interest in design and making games in Unity. Take a look at my
             recent projects</p>
-            <button className="project-button"><a href="#projects">PROJECTS</a></button>
+            <button className="project-button "><a href="#projects">PROJECTS</a></button>
         </div>
       </div>
     </section>
